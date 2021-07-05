8b0751f0623254809a545e8dcdf0a216f50d719d
bbasung1 <bbasung1@daum.net>
2021-07-05 14:49:51 +0900

    alramtest(from codechacha.com)

--- app/src/main/java/com/example/firstpcproject/MainActivity.kt
+++ app/src/main/java/com/example/firstpcproject/MainActivity.kt
@@ -1,41 +1,113 @@
 package com.example.firstpcproject
 
 import android.app.AlarmManager
-import androidx.appcompat.app.AppCompatActivity
+import android.app.PendingIntent
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
-import android.view.View.OnClickListener
-import android.widget.TextView
-import android.widget.Button
-import android.app.DatePickerDialog
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.icu.util.GregorianCalendar
-import android.view.View
-import android.widget.DatePicker
-import java.util.Calendar
+import android.os.SystemClock
+import android.util.Log
+import android.widget.CompoundButton.OnCheckedChangeListener
+import android.widget.Toast
+import android.widget.ToggleButton
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
+
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button:Button=findViewById(R.id.btn1)
-        val text:TextView=findViewById(R.id.text)
-        /*button.setOnClickListener{
-            text.text = "현자타임."
-        }*/
-        var year:Int=0
+        val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
 
-        button.setOnClickListener {
-            val today = GregorianCalendar()
-            year = today.get(Calendar.YEAR)
-            val month: Int = today.get(Calendar.MONTH)
-            val date: Int = today.get(Calendar.DATE)
-            val dlg = DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener {
-                override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
-                    text.text = "${year}년 ${month+1}월 ${dayOfMonth}일"
+        val intent = Intent(this, AlarmReceiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(
+            this, AlarmReceiver.NOTIFICATION_ID, intent,
+            PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val onetimeAlarmToggle:ToggleButton=findViewById(R.id.onetimeAlarmToggle)
+        onetimeAlarmToggle.setOnCheckedChangeListener(OnCheckedChangeListener { _, isChecked ->
+            val toastMessage = if (isChecked) {
+                val triggerTime = (SystemClock.elapsedRealtime()
+                        + 60 * 1000)
+                alarmManager.set(
+                    AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                    triggerTime,
+                    pendingIntent
+                )
+                "Onetime Alarm On"
+            } else {
+                alarmManager.cancel(pendingIntent)
+                "Onetime Alarm Off"
+            }
+            Log.d(TAG, toastMessage)
+            Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show()
+        })
+
+        val periodicAlarmToggle:ToggleButton=findViewById(R.id.periodicAlarmToggle)
+        periodicAlarmToggle.setOnCheckedChangeListener(OnCheckedChangeListener { _, isChecked ->
+            val toastMessage: String = if (isChecked) {
+                val repeatInterval: Long = AlarmManager.INTERVAL_FIFTEEN_MINUTES
+                val triggerTime = (SystemClock.elapsedRealtime()
+                        + repeatInterval)
+                alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                    triggerTime, repeatInterval,
+                    pendingIntent)
+                "Periodic Alarm On"
+            } else {
+                alarmManager.cancel(pendingIntent)
+                "Periodic Alarm Off"
+            }
+            Log.d(TAG, toastMessage)
+            Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show()
+        })
+
+        val exactPeriodicAlarmToggle:ToggleButton=findViewById(R.id.exactPeriodicAlarmToggle)
+        exactPeriodicAlarmToggle.setOnCheckedChangeListener(OnCheckedChangeListener { _, isChecked ->
+            val toastMessage: String = if (isChecked) {
+                val repeatInterval: Long = 60*1000
+                val triggerTime = (SystemClock.elapsedRealtime()
+                        + repeatInterval)
+                alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                    triggerTime, repeatInterval,
+                    pendingIntent)
+                "Exact periodic Alarm On"
+            } else {
+                alarmManager.cancel(pendingIntent)
+                "Exact periodic Alarm Off"
+            }
+            Log.d(TAG, toastMessage)
+            Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show()
+        })
+
+        val realtimePeriodicAlarmToggle:ToggleButton=findViewById(R.id.realtimePeriodicAlarmToggle)
+        realtimePeriodicAlarmToggle.setOnCheckedChangeListener(OnCheckedChangeListener { _, isChecked ->
+            val toastMessage: String = if (isChecked) {
+                val repeatInterval: Long = 15 * 60 * 1000 // 15min
+                val calendar: Calendar = Calendar.getInstance().apply {
+                    timeInMillis = System.currentTimeMillis()
+                    set(Calendar.HOUR_OF_DAY, 20)
+                    set(Calendar.MINUTE, 25)
                 }
-            }, year, month, date)
-            dlg.show()
-        }
-        text.text="${year}"
+
+                alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,
+                    calendar.timeInMillis,
+                    repeatInterval,
+                    pendingIntent)
+                "Realtime periodic Alarm On"
+            } else {
+                alarmManager.cancel(pendingIntent)
+                "Realtime periodic Alarm Off"
+            }
+            Log.d(TAG, toastMessage)
+            Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show()
+        })
+
     }
+
 }
